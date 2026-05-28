fc211e258c1ec6847df37365d7cd4f76c794703a
Magda Chikhradze <magda292005@gmail.com>
2026-05-28 17:16:07 +0400

    localstorage todo

--- new2/main.js
+++ new2/main.js
@@ -6,14 +6,51 @@
 // localStorage.setItem("name");
 // localStorage.getItem();
 
-localStorage.setItem("theme", "dark");
-console.log(localStorage.getItem("theme"));
+// localStorage.setItem("theme", "dark");
+// console.log(localStorage.getItem("theme"));
 
-sessionStorage.setItem("theme", "white");
-console.log(sessionStorage.getItem("theme"));
+// sessionStorage.setItem("theme", "white");
+// console.log(sessionStorage.getItem("theme"));
 
 // localstorage - mudmivia sanam shen ar dacer sacinaagmdegos
 
 // localStorage.clear();
 
 // localStorage.removeItem("theme");
+
+// TODO APP
+
+const input = document.getElementById("text");
+const addBtn = document.querySelector(".add");
+const listContainer = document.querySelector(".list");
+
+let todos = [];
+
+addBtn.addEventListener("click", () => {
+  let todo = {
+    id: crypto.randomUUID(),
+    text: input.value,
+  };
+
+  todos.push(todo);
+
+  localStorage.setItem("todosArr", JSON.stringify(todos));
+
+  render();
+});
+
+function render() {
+  todos.forEach((e) => {
+    const li = document.createElement("li");
+    li.innerHTML = ` <li>
+      <input type="checkbox" id="todoCkeck" />
+      <label for="todoCkeck">${e.text}</label>
+    </li>`;
+
+    listContainer.append(li);
+
+    todos = [];
+  });
+}
+
+render();
