b3f5d38e7c9e6b1ce0f21a50e260cdae480b530d
Magda Chikhradze <magda292005@gmail.com>
2026-05-28 16:33:49 +0400

    getitem, setitem

---
+++ new2/main.js
@@ -0,0 +1,19 @@
+//F12 opens application
+// logcal storage
+//session storage
+//cookies
+
+// localStorage.setItem("name");
+// localStorage.getItem();
+
+localStorage.setItem("theme", "dark");
+console.log(localStorage.getItem("theme"));
+
+sessionStorage.setItem("theme", "white");
+console.log(sessionStorage.getItem("theme"));
+
+// localstorage - mudmivia sanam shen ar dacer sacinaagmdegos
+
+// localStorage.clear();
+
+// localStorage.removeItem("theme");
